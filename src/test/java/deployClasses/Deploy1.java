bc9025d1a2f4a4ebe3d002aa1a1afab9124d7436
Shridhar Giri <Shridhar.Giri@ivycomptech.com>
2023-08-15 16:18:01 +0530

    Commititng deployJobs

---
+++ src/test/java/deployClasses/Deploy1.java
@@ -0,0 +1,13 @@
+package deployClasses;
+
+import org.testng.annotations.Test;
+
+public class Deploy1 {
+	
+	@Test
+	public void deploytest()
+	{
+		System.out.println("Completed class 1 for deployment");
+	}
+
+}
---
+++ src/test/java/deployClasses/Deploy2.java
@@ -0,0 +1,13 @@
+package deployClasses;
+
+import org.testng.annotations.Test;
+
+public class Deploy2 {
+	
+	@Test
+	public void deploytest()
+	{
+		System.out.println("Completed deployment on qa2 environement");
+	}
+
+}
